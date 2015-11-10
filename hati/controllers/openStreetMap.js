8e8b0d02c27335c931966f6d4b2779eff9295c6c
Raphael Gruber <csaq1131@zid-t9pc1521.uibk.ac.at>
2015-11-10 08:41:52 +0100

    changes

--- hati/controllers/openStreetMap.js
+++ hati/controllers/openStreetMap.js
@@ -7,11 +7,11 @@ function getRoute(startLat, startLon, endLat, endLon, callback) {
 		url: 'http://www.yournavigation.org/api/1.0/gosmore.php',
 		method: 'GET',
     qs: {format: 'kml', flat: startLat, flon: startLon,
-      tlat: endLat, tlon: endLon, v: 'foot', fast: '1', layer:'mapnik'},
+      tlat: endLat, tlon: endLon, v: 'foot', fast: '1', layer:'mapnik', instructions: '1'},
 	}, callback);
 }
 
-getRoute('52.215676','=5.963946','52.2573','6.1799',function(error, response, body) {
+getRoute('52.215676','5.963946','52.2573','6.1799',function(error, response, body) {
 	if(error) {
 		console.log(error);
   } else {
