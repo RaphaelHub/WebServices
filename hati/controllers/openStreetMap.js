b48aa3c2edcd8aea43ff9cd1b16bb6c4c2076d24
RaphaelHub <raphael-gruber@hotmail.com>
2015-11-10 18:39:21 +0100

    change to json object

--- hati/controllers/openStreetMap.js
+++ hati/controllers/openStreetMap.js
@@ -6,8 +6,8 @@ function getRoute(startLat, startLon, endLat, endLon, callback) {
 	request({
 		url: 'http://www.yournavigation.org/api/1.0/gosmore.php',
 		method: 'GET',
-    qs: {format: 'kml', flat: startLat, flon: startLon,
-      tlat: endLat, tlon: endLon, v: 'foot', fast: '1', layer:'mapnik', instructions: '1'},
+    qs: {format: 'geojson', flat: startLat, flon: startLon,
+      tlat: endLat, tlon: endLon, v: 'foot', fast: '1', layer:'mapnik'},
 	}, callback);
 }
 
@@ -15,6 +15,8 @@ getRoute('52.215676','5.963946','52.2573','6.1799',function(error, response, bod
 	if(error) {
 		console.log(error);
   } else {
-    console.log(body);
+		var route = JSON.parse(body);
+    var coor = route.coordinates;
+		console.log(coor);
 	}
 });
