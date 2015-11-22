061f84f36957230840a40a702755d491439d1dc6
RaphaelHub <raphael-gruber@hotmail.com>
2015-11-22 23:34:48 +0100

    implemeted with Promises

--- hati/controllers/openStreetMap.js
+++ hati/controllers/openStreetMap.js
@@ -1,22 +1,26 @@
 var request = require('request');
 var _ = require('lodash');
+var Q = require('q');
 
-
-function getRoute(startLat, startLon, endLat, endLon, callback) {
+function getRoute(startLat, startLon, endLat, endLon) {
+	var deferred = Q.defer();
 	request({
 		url: 'http://www.yournavigation.org/api/1.0/gosmore.php',
 		method: 'GET',
     qs: {format: 'geojson', flat: startLat, flon: startLon,
       tlat: endLat, tlon: endLon, v: 'foot', fast: '1', layer:'mapnik'},
-	}, callback);
+	}, function(error, response, body) {
+		if(error) {
+			deferred.reject(error);
+	  } else {
+			var route = JSON.parse(body);
+			  deferred.resolve(route);
+		}
+	});
+	return deferred.promise;
 }
 
-getRoute('52.215676','5.963946','52.2573','6.1799',function(error, response, body) {
-	if(error) {
-		console.log(error);
-  } else {
-		var route = JSON.parse(body);
-    var coor = route.coordinates;
-		console.log(coor);
-	}
+
+getRoute('52.215676','5.963946','52.2573','6.1799').then(function(repos) {
+	console.log(repos.coordinates);
 });
