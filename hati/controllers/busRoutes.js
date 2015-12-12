bb02290912e180b670922df2a4c91ed569d1aed2
philipphaeusle <philipp.haeusle@hotmail.com>
2015-12-12 16:16:21 +0100

    refactored and added functions. problem: sometimes graph is empty

--- hati/controllers/busRoutes.js
+++ hati/controllers/busRoutes.js
@@ -1,74 +1,18 @@
 var request = require('request');
+var map = require('../controllers/openStreetMap.js');
 var Graph = require('../models/graph.js');
 var parser= require('xml2json');
 var _ = require('lodash');
 var Q = require('q');
 
 
-function getJSON(type,id) { //macht api Anfragen
-	var deferred = Q.defer();
-	request({
-		url: 'http://api.openstreetmap.org/api/0.6/'+type+'/'+id,
-		qs: {},
-		method: 'GET',
-	}, function(error, response, body) {
-		if(error) {
-			deferred.reject(error);
-	  } else {
-			  deferred.resolve(JSON.parse(parser.toJson(body)));
-		}
-	});
-	return deferred.promise;
-}
-
-function getRelations() { //gibt alle Relationen einer Buslinie zurück.
-	var deferred = Q.defer();
-	var ids=[71787,359908,71785,89518,359910,4201061,359911,359910,3977518,382568,4286872,4286873,3979143,2589450,2589451,3978031,379544,2589452,113811,379533,3998983,4286874,379545,3975811];
-	var relations=[];
-	var promises=[];
-	for(var i=0; i<ids.length; i++){
-		promises.push(getJSON('relation',ids[i]));
-	}
-	for(var i=0; i<promises.length; i++){
-		promises[i].then(function(bus){
-			if(_.some(bus.osm.relation.member,{type: 'relation'})){
-				var temp =_.filter(bus.osm.relation.member, {type: 'relation'});
-				_.forEach(temp, function(elem){
-					relations.push(elem.ref);
-				});
-				//console.log(_.find(bus.osm.relation.tag, {k: 'name'}).v +"  id:"+ bus.osm.relation.id);
-			}
-		});
-	}
-	Q.all(promises).done(function() {
-				deferred.resolve(relations);
-					//console.log("Hello from the other side");
-	});
-	return deferred.promise;
-}
-
-function getNodes(relId){ //gibt alle Bushaltestellen einer Relation zurück.
-	var deferred = Q.defer();
-	var promises=[];
-	getJSON('relation',relId).then(function(rel){
-		var temp =_.filter(rel.osm.relation.member, {type: 'node', role:'stop'});
-		for(var i=0; i<temp.length; i++){
-			promises.push(getJSON('node', temp[i].ref));
-		}
-		Q.all(promises).then(function(nodes) {
-			deferred.resolve(nodes);
-		});
-	});
-	return deferred.promise;
-}
-
 function fillGraph(){
 	var deferred = Q.defer();
 	var promises=[];
 	var graph = new Graph();
-	getRelations().then(function(relations){
+	map.getRelations([71787,359908,71785,89518,359910,4201061,359911,359910,3977518,382568,4286872,4286873,3979143,2589450,2589451,3978031,379544,2589452,113811,379533,3998983,4286874,379545,3975811]).then(function(relations){
 		for(var i=0; i<relations.length;i++){
-			promises.push(getNodes(relations[i]));
+			promises.push(map.getRelNodes(relations[i]));
 		}
 		for(var i=0; i<promises.length; i++){
 			promises[i].then(function(nodes){
@@ -103,5 +47,7 @@ function fillGraph(){
 }
 
 fillGraph().then(function(graph){
-		graph.print();
+		console.log("1");
+		graph.print(); //gibt nur manchmal eine Ausgabe??
+		console.log("2");
 })
--- hati/controllers/openStreetMap.js
+++ hati/controllers/openStreetMap.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var _ = require('lodash');
 var Q = require('q');
+var parser= require('xml2json');
 
 exports.getRoute = function(startCoord, endCoord) {
 	var start = startCoord.toString().split(',');
@@ -21,3 +22,101 @@ exports.getRoute = function(startCoord, endCoord) {
 	});
 	return deferred.promise;
 }
+
+function getObject(type,id) { //macht api Anfragen
+	var deferred = Q.defer();
+	request({
+		url: 'http://api.openstreetmap.org/api/0.6/'+type+'/'+id,
+		qs: {},
+		method: 'GET',
+	}, function(error, response, body) {
+		if(error) {
+			deferred.reject(error);
+	  } else {
+			  deferred.resolve(JSON.parse(parser.toJson(body)));
+		}
+	});
+	return deferred.promise;
+}
+
+
+exports.getRelNodes=function(relId){ //gibt alle Bushaltestellen einer Relation zurück.
+	var deferred = Q.defer();
+	var promises=[];
+	getObject('relation',relId).then(function(rel){
+		var temp =_.filter(rel.osm.relation.member, {type: 'node', role:'stop'});
+		for(var i=0; i<temp.length; i++){
+			promises.push(getObject('node', temp[i].ref));
+		}
+		Q.all(promises).then(function(nodes) {
+			deferred.resolve(nodes);
+		});
+	});
+	return deferred.promise;
+}
+
+exports.getRelations=function(ids) { //gibt alle Relationen einer Buslinie zurück.
+	var deferred = Q.defer();
+	var relations=[];
+	var promises=[];
+	for(var i=0; i<ids.length; i++){
+		promises.push(getObject('relation',ids[i]));
+	}
+	for(var i=0; i<promises.length; i++){
+		promises[i].then(function(bus){
+			if(_.some(bus.osm.relation.member,{type: 'relation'})){
+				var temp =_.filter(bus.osm.relation.member, {type: 'relation'});
+				_.forEach(temp, function(elem){
+					relations.push(elem.ref);
+				});
+				//console.log(_.find(bus.osm.relation.tag, {k: 'name'}).v +"  id:"+ bus.osm.relation.id);
+			}
+		});
+	}
+	Q.all(promises).done(function() {
+				deferred.resolve(relations);
+					//console.log("Hello from the other side");
+	});
+	return deferred.promise;
+}
+
+function getNodes(ids){
+	var deferred = Q.defer();
+	var promises=[];
+	for(var i=0; i<ids.length; i++){
+		promises.push(getObject('node',ids[i]));
+	}
+	Q.all(promises).then(function(nodes) {
+		deferred.resolve(nodes);
+	});
+return deferred.promise;
+}
+
+
+function getNode(id){
+		return getObject('node', id);
+}
+
+function getRelation(id){
+		return getObject('relation', id);
+}
+
+
+function distance(lat1,lat2,lon1,lon2){
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var radlon1 = Math.PI * lon1/180
+	var radlon2 = Math.PI * lon2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	dist = dist * 1.609344
+	return dist;
+}
+
+getNodes([1773746307,3688277404]).then(function(x){
+	console.log(x);
+});
